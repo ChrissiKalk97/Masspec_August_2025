ed42bf2238ba58ed8ad4fd59199560eb53b17f3b
ChrissiKalk97 <kalk@med.uni-frankfurt.de>
2025-08-26 13:47:22 +0200

    compare normalized and scaled abundance

--- compare_abundance_across_samples.py
+++ compare_abundance_across_samples.py
@@ -1,4 +1,6 @@
+import sys
 import os
+import logging
 import pandas as pd
 import argparse
 from scipy import stats
@@ -7,42 +9,58 @@ import numpy as np
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="Process peptide and SO ID mapping files.")
+        description='Process peptide and SO ID mapping files.')
 
     parser.add_argument(
-        "--cm_unique_peps_csv",
+        '--cm_unique_peps_csv',
         required=True,
-        help="."
+        help='.'
     )
 
     parser.add_argument(
-        "--huvec_unique_peps_csv",
-        help="."
+        '--huvec_unique_peps_csv',
+        required=True,
+        help='.'
+    )
+
+    parser.add_argument(
+        '--scaled',
+        required=True,
+        help='.'
     )
     return parser.parse_args()
 
 
-def main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict):
+def main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict, scaled):
+    log_dir = os.path.dirname(cm_unique_peps_csv)
+
+    log_file = open(os.path.join(
+        log_dir, f'test_{scaled}_differences_in_exp_unique_peptides.out'), 'w')
+
+    # Redirect only stdout (used by print), but leave stderr alone
+    sys.stdout = log_file
+
     cm_unique_peptdies_df = pd.read_csv(cm_unique_peps_csv, index_col=0)
     huvec_unique_peptdies_df = pd.read_csv(huvec_unique_peps_csv, index_col=0)
 
     ################################################################################
     # Compare CM NMD vs DMSO by wilcoxon signed rank test
     ################################################################################
-    print('Test differences in expression of unique peptides for CM NMD vs DMSO')
+    print(
+        f'Test differences in {scaled} expression of unique peptides for CM NMD vs DMSO')
     wilcoxon_signed_test_ps = []
     for unique_peptide in cm_unique_peptdies_df.index:
 
         wilcox_signed_result = stats.wilcoxon(
             x=np.array(
-                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 130C Sample',
-                                                           f'Abundances Normalized F1 131N Sample',
-                                                           f'Abundances Normalized F1 131C Sample']].copy(),
+                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 130C Sample',
+                                                           f'Abundances {scaled} F1 131N Sample',
+                                                           f'Abundances {scaled} F1 131C Sample']].copy(),
                 dtype=float),
             y=np.array(
-                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 {cm_dict['130C']} Sample',
-                                                           f'Abundances Normalized F1 {cm_dict['131N']} Sample',
-                                                           f'Abundances Normalized F1 {cm_dict['131C']} Sample']].copy(),
+                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 {cm_dict['130C']} Sample',
+                                                           f'Abundances {scaled} F1 {cm_dict['131N']} Sample',
+                                                           f'Abundances {scaled} F1 {cm_dict['131C']} Sample']].copy(),
                 dtype=float),
             alternative='greater')
         # print(wilcox_signed_result)
@@ -61,20 +79,21 @@ def main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict):
     ################################################################################
     # Compare CM Hypo vs DMSO by wilcoxon signed rank test
     ################################################################################
-    print('Test differences in expression of unique peptides for CM Hypo vs DMSO')
+    print(
+        f'Test differences in {scaled} expression of unique peptides for CM Hypo vs DMSO')
     wilcoxon_signed_test_ps = []
     for unique_peptide in cm_unique_peptdies_df.index:
 
         wilcox_signed_result = stats.wilcoxon(
             x=np.array(
-                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 127N Sample',
-                                                           f'Abundances Normalized F1 128N Sample',
-                                                           f'Abundances Normalized F1 129N Sample']].copy(),
+                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 127N Sample',
+                                                           f'Abundances {scaled} F1 128N Sample',
+                                                           f'Abundances {scaled} F1 129N Sample']].copy(),
                 dtype=float),
             y=np.array(
-                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 {cm_dict['127N']} Sample',
-                                                           f'Abundances Normalized F1 {cm_dict['128N']} Sample',
-                                                           f'Abundances Normalized F1 {cm_dict['129N']} Sample']].copy(),
+                cm_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 {cm_dict['127N']} Sample',
+                                                           f'Abundances {scaled} F1 {cm_dict['128N']} Sample',
+                                                           f'Abundances {scaled} F1 {cm_dict['129N']} Sample']].copy(),
                 dtype=float),
             alternative='greater')
         # print(wilcox_signed_result)
@@ -93,20 +112,21 @@ def main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict):
     ################################################################################
     # Compare HUVEC hypo vs DMSO by wilcoxon signed rank test
     ################################################################################
-    print('Test differences in expression of unique peptides for HUVEC Hypo vs DMSO')
+    print(
+        f'Test differences in  {scaled} expression of unique peptides for HUVEC Hypo vs DMSO')
     wilcoxon_signed_test_ps = []
     for unique_peptide in huvec_unique_peptdies_df.index:
 
         wilcox_signed_result = stats.wilcoxon(
             x=np.array(
-                huvec_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 127 Sample',
-                                                              f'Abundances Normalized F1 129 Sample',
-                                                              f'Abundances Normalized F1 131 Sample']].copy(),
+                huvec_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 127 Sample',
+                                                              f'Abundances {scaled} F1 129 Sample',
+                                                              f'Abundances {scaled} F1 131 Sample']].copy(),
                 dtype=float),
             y=np.array(
-                huvec_unique_peptdies_df.loc[unique_peptide, [f'Abundances Normalized F1 {huvec_dict['127']} Sample',
-                                                              f'Abundances Normalized F1 {huvec_dict['129']} Sample',
-                                                              f'Abundances Normalized F1 {huvec_dict['131']} Sample']].copy(),
+                huvec_unique_peptdies_df.loc[unique_peptide, [f'Abundances {scaled} F1 {huvec_dict['127']} Sample',
+                                                              f'Abundances {scaled} F1 {huvec_dict['129']} Sample',
+                                                              f'Abundances {scaled} F1 {huvec_dict['131']} Sample']].copy(),
                 dtype=float),
             alternative='greater')
         # print(wilcox_signed_result)
@@ -123,10 +143,11 @@ def main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict):
           len([p for p in wilcoxon_signed_test_ps if p < 0.5]))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_arguments()
     cm_unique_peps_csv = args.cm_unique_peps_csv
     huvec_unique_peps_csv = args.huvec_unique_peps_csv
+    scaled = args.scaled
 
     # cm_unique_peps_csv = '/Users/christina/Documents/own_data/Masspec/analysis_results/cm_unique_so_peptides_after_proteome_filtering.csv'
     # huvec_unique_peps_csv = '/Users/christina/Documents/own_data/Masspec/analysis_results/huvec_unique_so_peptides_after_proteome_filtering.csv'
@@ -143,4 +164,4 @@ if __name__ == "__main__":
         '127': '126', '129': '128', '131': '130',
     }
 
-    main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict)
+    main(cm_unique_peps_csv, cm_dict, huvec_unique_peps_csv, huvec_dict, scaled)
